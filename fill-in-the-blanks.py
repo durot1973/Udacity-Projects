f8585c00d513f38bd6053238bcb40aeb981c410d
Durotimi Taiwo <31576028+durot1973@users.noreply.github.com>
2017-12-16 08:12:17 +0000

    Improved Readability

--- fill-in-the-blanks.py
+++ fill-in-the-blanks.py
@@ -1,4 +1,4 @@
-
+x
 # For this project, you'll be building a Fill-in-the-Blanks quiz.
 # Your quiz will prompt a user with a paragraph containing several blanks.
 # The user should then be asked to fill in each blank appropriately to complete the paragraph.
@@ -14,24 +14,30 @@ import string
 # first we define the functions necessary for the code to run.
 
 def introduction():
-	'''This function introduces the program and points in the next direction'''
+	'''
+	Behaviour: Asks the user to pick a level in the game
+	Input: None
+	Output: Level the user chooses
+	'''
 	level_chosen = ''
+	levels = ['easy', 'medium', 'hard']
 
-	while level_chosen !='easy' or level_chosen != 'medium' or level_chosen !='hard':
+	while level_chosen not in levels:
 		print 'Please select a game difficulty by typing it in!'
 		print 'Possible choices include easy, medium and hard'
-		levels = ['easy', 'medium', 'hard']
 		level_chosen = raw_input()
 		level_chosen = level_chosen.lower()
-		if level_chosen in levels:
-			return level_chosen
-		else:
+		if level_chosen not in levels:
 			print "That's not an option!"
+	else:
+		return level_chosen
 
 
 def isSubstituted(paragraph):
 	'''
-	Checks if the words have been substituted into the paragraphs
+	Behaviour: Checks if all word have been replaced in the string
+	Input: A paragragh
+	Output: True or False depending on the strings
 	'''
 	if '__' in paragraph:
 			return False
@@ -40,9 +46,9 @@ def isSubstituted(paragraph):
 
 def toSubstitute(replacement, space, paragraph):
 	'''
-	This function replaces space with word in the paragraph
-	replacement is the the word the user has inputed that is correct
-	space is what is to be substituted
+	Behaviour: Substitutes the blanks in the paragraph with the user's input
+	Input: User's guess, the blank to be substituted and the paragraph
+	Output: paragraph without the blank
 	'''
 	assert '__' in space
 	if space in paragraph:
@@ -52,32 +58,35 @@ def toSubstitute(replacement, space, paragraph):
 
 def identifySubstitute(paragraph):
 	'''
-	This is to be used to find the next word to be substituted
-	e.g __1__.
+	Behaviour: Locates the next blank space
+	Input: Paragraph
+	Output: The blank space as represented
 	'''
 	index = paragraph.index('___')
 	substitute_length = 7
 	return paragraph[index: index + substitute_length]
 
 
-def game(paragraph, answers, level_chosen):
-	'''This function accepts values from the either easy, medium or hard and runs with that giving the user feedback 
-	for each guess made'''
-	guesses, user_input, substitute_paragraph, words_to_be_substituted = 5, '', paragraph[:], answers.copy()
+def game(level_chosen, paragraph, answers):
+	'''
+	Behaviour: Runs the game for the player.
+	Input: The level chosen, the paragraph based on the level choosen, and the list of answers.
+	Output: None
+	'''
+	guesses, user_input, substitute_paragraph, index = 5, '', paragraph[:], 0
 	print 'You have chosen ' + level_chosen + '!' + '\n  ' + '\nYou will get ' + str(guesses) + ' guesses per problem' + '\n '
 	
 	while not isSubstituted(paragraph):
-		substitute_word = identifySubstitute(paragraph)
+		substitute_word, last_guess = identifySubstitute(paragraph), 1
 		print 'The current paragraph reads as such:' + '\n' + substitute_paragraph + '\n' 
 		user_input = (raw_input('What should be substituted in for ' + substitute_word + '?')).lower()
 		
-		if user_input in words_to_be_substituted:
+		if user_input in answers[index]:
 			print '\nCorrect!'
-			substitute_paragraph, paragraph = toSubstitute(user_input, substitute_word, paragraph), toSubstitute(user_input, substitute_word, paragraph)
-			words_to_be_substituted.discard(user_input)
+			substitute_paragraph, paragraph, index = toSubstitute(user_input, substitute_word, paragraph), toSubstitute(user_input, substitute_word, paragraph), index + 1
 		else:
 			guesses -= 1
-			if guesses < 1:
+			if guesses < last_guess:
 				break
 			else:
 				print "\nThat's not the correct answer: Let's try again; you have %g try left" %guesses
@@ -97,7 +106,7 @@ game_levels = {'easy': { 'phrase': '''A common first thing to do in a language i
  	odd to do something in a Turing complete language that can be done 
  	even more easily with an ___4___ file in a browser, but it is a step
  	 in learning ___2___ and that is really it's purpose''',
- 	  'answers': {'world', 'python', 'print', 'html'}},
+ 	  'answers': ('world', 'python', 'print', 'html')},
  
  'medium': {'phrase': '''A ___1___ is created with the def keyword. You specify 
  	the inputs a ___1___ takes by adding ___2___ separated by commas between
@@ -105,15 +114,20 @@ game_levels = {'easy': { 'phrase': '''A common first thing to do in a language i
  	 the value to return. ___2___ can be standard data types such as string,
  	 number, dictionary, tuple, and ___4___ or can be more complicated such
  	 as objects and lambda functions.''', 
- 	'answers': {'function', 'argument', 'none', 'list'}},
+ 	'answers': ('function', 'argument', 'none', 'list')},
  
  'hard': {'phrase': '''When you create a ___1___, certain ___2___ are 
  	automatically generated for you if you don't make them manually. An 
  	object is an ___3___ of a class is. Binary operators can also be 
  	created for the class e.g ___4___ which lets '+' be used on the class''', 
- 	'answers': {'class', 'methods', 'instance', 'add'}}}
+ 	'answers': ('class', 'methods', 'instance', 'add')}}
 
 def progress_game(level_chosen):
+	'''
+	Behaviour: Starts the game and sets up the paragraph and the answer for the game.
+	Input: level_chosen
+	Output: None
+	'''
 	game_level = game_levels[level_chosen]
 	return game(game_level['phrase'], game_level['answers'], level_chosen)
 
